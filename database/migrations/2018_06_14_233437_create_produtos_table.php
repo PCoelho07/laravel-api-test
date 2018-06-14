f5509dd160851633497751932a40e9bff8e034a5
Pedro Coelho <pedro_coelho_pv@hotmail.com>
2018-06-14 20:53:11 -0300

    Add product model and migration

---
+++ database/migrations/2018_06_14_233437_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->float('valor_compra', 8, 2);
+            $table->float('valor_revenda', 8, 2);
+            $table->string('ativo');
+            $table->string('imagem_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
