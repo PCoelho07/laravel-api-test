bf0dbd8a1663013eb67432c5aa3fe49e8bc9f754
Pedro Coelho <pedro_coelho_pv@hotmail.com>
2018-06-14 21:34:42 -0300

    Adds firsts endpoints to products

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('products')->group(function () {
+
+    Route::get('/', 'ProdutoController@index');
+    Route::get('/{id}','ProdutoController@show');
+    
+
+
 });
