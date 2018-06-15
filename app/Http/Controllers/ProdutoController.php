abb63035439ec12d503c0b7858423eaaa3f7f7c1
Pedro Coelho <pedro_coelho_pv@hotmail.com>
2018-06-14 21:35:04 -0300

    Add method controller to products endpoints

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Produto;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produtos = Produto::paginate();
+
+        return response()->json([
+            'result' => $produtos
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $produto = Produto::find($id);
+
+        if(! $produto) {
+            return response()->json([
+                'result' => 'Resource not found!'
+            ], 404);
+        }
+
+        return response()->json([
+                'result' => $produto
+            ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
